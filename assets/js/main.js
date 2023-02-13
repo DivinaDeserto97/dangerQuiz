5ebdec31f93b1af66ecf39fb54dcfd508a893180
Divina Deserto <117446023+DivinaDeserto97@users.noreply.github.com>
2023-02-13 10:09:35 +0100

    panik

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,7 +15,7 @@ function deleteAllCookies() {
         const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
         document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT';
     }
-    window.location.href = '../index.php';
+    window.location.href = 'index.php';
 }
 
 // NAVIGATION ------------------------------------------------------------------
