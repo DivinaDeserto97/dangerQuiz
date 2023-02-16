6ba2d7bd4de4644fb5b67cee3d68067d50c62307
MayckellP <mayckell-perez@hotmail.com>
2023-02-16 13:32:19 +0100

    icons

--- question.php
+++ question.php
@@ -6,6 +6,7 @@
     <meta charset='UTF-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <link rel="shortcut icon" href="/assets/images/logos/danger-quiz-high-resolution-logo-white-on-transparent-background.svg" type="image/x-icon">
     <title>Question</title>
 
     <?php require('./includes/srcUp.php'); ?>
